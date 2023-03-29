4c9d7febb1f9046aa518138b27b54d3079038abd
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-03-29 21:55:47 +0800

    fix error on command create_due_invoices

--- shop/tasks/due_products.py
+++ shop/tasks/due_products.py
@@ -1,30 +1,44 @@
+from datetime import timedelta
 from django.utils import timezone
-from shop.models import Customer, Product, Invoice
+from shop.models import Product, Invoice
 
 
 def get_due_products(brand):
-    today = timezone.now().date()
-    due_products = Product.objects.filter(
-        brand=brand,
-        customer__is_terminated=False,
-        next_billing_date__lte=today
-    )
+    due_products = []
+    now = timezone.now()
+    print(f"Today: {now.date()}")
+    print(f"Processing products for brand {brand.brand_name}:")
+    for product in Product.objects.filter(brand=brand, is_active=True):
+        print(f"Product {product.pk}: next_billing_date={product.next_billing_date}")
+        if product.next_billing_date.date() <= now.date():
+            due_products.append(product)
+            print(f"  -> Due product: {product.pk}")
     return due_products
 
 
 def create_invoice_for_product(due_products):
     invoices_created = []
+
     for product in due_products:
         invoice = Invoice.objects.create(
+            invoice_number=f"INVOICE{Invoice.objects.count() + 1:04d}",
             customer=product.customer,
-            description=f"Invoice for {product.product_number}",
+            description=f"Invoice for {product.description}",
             product=product,
-            total_price=product.price
+            total_price=product.price,
         )
-        if invoice:
-            product.previous_billing_date = invoice.created_at
-            product.next_billing_date = invoice.created_at + timezone.timedelta(days=30)
-            product.save()
-            invoices_created.append(invoice)
+        invoices_created.append(invoice)
+
+        print(f"Invoice created: {invoice}")
+        print(f"Old next_billing_date: {product.next_billing_date}")
+        print(f"Old previous_billing_date: {product.previous_billing_date}")
+
+        product.previous_billing_date = timezone.now()
+        product.next_billing_date = timezone.now() + timedelta(days=30)
+        product.save()
+
+        print(f"New next_billing_date: {product.next_billing_date}")
+        print(f"New previous_billing_date: {product.previous_billing_date}")
+        print("---")
 
     return invoices_created
